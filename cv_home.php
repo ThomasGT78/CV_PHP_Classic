5b6d74f2741d8164a5d79b547dbb3bfb34d41cd2
thomas <thomas.garot@gmail.com>
2023-08-20 20:07:24 +0200

    First commit

---
+++ cv_home.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link type="text/css" rel="stylesheet" href="css/stylesheet.css" />
+
+    <title>CV Home</title>
+</head>
+<body>
+    <h1>Gérer Mon CV</h1>
+    <h4>Mon site perso, venez me découvrir</h4>
+    <div id="nav"></div>
+    
+
+    <script src="js/home.js"></script>
+</body>
+</html>
