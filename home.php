8d45064f3a986d0a0a81c71ec98305787a3ba56a
thomas <thomas.garot@gmail.com>
2023-08-23 17:39:51 +0200

    Mise en route

--- home.php
+++ home.php
@@ -1,4 +1,6 @@
+<?php
 
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -7,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link type="text/css" rel="stylesheet" href="css/stylesheet.css" />
+    <link type="text/css" rel="stylesheet" href="style/stylesheet.css" />
 
     <title>CV Home</title>
 </head>
